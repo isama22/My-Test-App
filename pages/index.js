4978a4caacc9067153fd048b0a22142b90809059
isama22 <emarti17@artic.edu>
2021-06-21 00:51:18 -0500

    completed mini video walk through

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Heading, Page, Button } from "@shopify/polaris";
+import { Component } from "react";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+class Index extends Component {
+
+  state = {
+    open: false
+  }
+
+  render() {
+    return (
+      <Page
+        title='Product selector'
+        primaryAction={{
+          content: 'Select Products',
+          onAction: () => this.setState({ open: true })
+
+        }}
+      >
+        <ResourcePicker
+          resourceType='Product'
+          open={this.state.open}
+          onCancel={ () => this.setState({ open: false })}
+          onSelection={ (resources) => this.handleSelection(resources)}
+        />
+      </Page>
+    )
+  }
+  handleSelection = (resources) => {
+    const IdFromResources = resources.selection.map((product) => product.id)
+    this.setState({ open: false })
+    console.log(IdFromResources)
+  }
+}
 
 export default Index;
